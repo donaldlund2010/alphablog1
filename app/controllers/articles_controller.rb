800760cbb44bff9115304dbb634ab571af320911
Donald Lundgren <donaldlund2017@gmail.com>
2019-01-10 09:19:00 +0000

    last changes for morning

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     def index 
         @articles = Article.all
     end
@@ -7,7 +9,7 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def edit
-       @article = Article.find(params[:id]) 
+        
     end
     
     def create
@@ -20,7 +22,7 @@ class ArticlesController < ApplicationController
         end
     end
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Article was updated"
             redirect_to article_path(@article)
@@ -29,15 +31,18 @@ class ArticlesController < ApplicationController
         end
     end
     def show
-       @article = Article.find(params[:id])
+      
     end
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         flash[:notice] = "Article was deleted"
         redirect_to articles_path
     end
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
         def article_params
             params.require(:article).permit(:title, :description)
     end
