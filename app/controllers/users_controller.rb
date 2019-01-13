9bcaed92a084e4c7a14c5fd307ca7985df1f8832
Donald Lundgren <donaldlund2017@gmail.com>
2019-01-13 20:11:00 +0000

    Make better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
 
 before_action :set_user, only: [:edit, :update, :show]
 
-before_action :require_same_user, only: [:edit, :update]
+before_action :require_same_user, only: [:edit, :update, :destroy]
+
+before_action :require_admin, only: [:destroy]
 
 def index
 
@@ -62,6 +64,19 @@ def show
 
 end
 
+def destroy
+
+@user = User.find(params[:id])
+
+@user.destroy
+
+flash[:danger] = "User and all articles created by user have been deleted"
+
+redirect_to users_path
+
+end
+
+private
 
 def user_params
 
@@ -77,7 +92,7 @@ end
 
 def require_same_user
 
-if current_user != @user
+if current_user != @user and !current_user.admin?
 
 flash[:danger] = "You can only edit your own account"
 
@@ -86,4 +101,17 @@ redirect_to root_path
 end
 
 end
+
+def require_admin
+
+if logged_in? and !current_user.admin?
+
+flash[:danger] = "Only admin users can perform that action"
+
+redirect_to root_path
+
+end
+
+end
+
 end
